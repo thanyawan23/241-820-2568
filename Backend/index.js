3c90152b3f394223ea8316812ec1c73f4fe54f9c
Thanyawan23 <thanyawan.b@ku.th>
2026-01-26 17:05:59 +0700

    update html

---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+//ทำการ import http เพื่อสร้าง server
+const http = require('http');
+const host = 'localhost';
+const port = 8000;
+
+//กำหนดค่าเริ่มต้นของ server เมื่อเปิดใช้งานเว็บผ่าน browser ที่ localhost:8000
+const requestListener = function(req,res) {
+    res.writeHead(200);
+    res.end('My First Server!');
+}
+
+//run server
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
