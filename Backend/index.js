826fe5b8ebfe1511c1b3ebd42dd29e8928fbc20c
Thanyawan23 <thanyawan.b@ku.th>
2026-02-16 16:47:46 +0700

    update html

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,72 @@
-//ทำการ import http เพื่อสร้าง server
-const http = require('http');
-const host = 'localhost';
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 const port = 8000;
 
-//กำหนดค่าเริ่มต้นของ server เมื่อเปิดใช้งานเว็บผ่าน browser ที่ localhost:8000
-const requestListener = function(req,res) {
-    res.writeHead(200);
-    res.end('My First Server!');
-}
+app.use(bodyParser.json());
 
-//run server
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+let users = []
+let counter = 1;
+
+
+//path = /user
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+//path = POST/user
+app.post('/user', (req, res) => {
+    let user = req.body;
+    user.id = counter
+    counter += 1;
+    users.push(user);
+    res.json({
+        message: 'User added successfully', 
+        user: user});
+});
+
+//path = PUT/user/:id
+app.put('/user/:id', (req, res) => {
+    let id = req.params.id;
+    let updatedUser = req.body;
+    //หา users จาก id
+    let selectedUserIndex = users.findIndex(user => user.id == id)
+    //update users นั้น
+    if (updatedUser.name) {
+        users[selectedUserIndex].name = updatedUser.name
+    }
+    else {
+        users[selectedUserIndex].name = users[selectedUserIndex].name
+    }
+    //ส่ง response กลับไปว่า update users ที่เลือกสำเร็จแล้ว
+    res.json({
+        message: `User with id ${id} updated successfully`,
+        data : {
+            user: updatedUser,
+            indexUpdated: selectedUserIndex
+        }
+    })
+})
+
+//path = DELETE/user/:id
+app.delete('/users/:id', (req, res) => { 
+    let id = req.params.id;
+    let selectedUserIndex = users.findIndex(user => user.id == id)
+    if (selectedUserIndex !== -1) {
+        users.splice(selectedUserIndex, 1);
+        res.json({
+            message: `User with id ${id} deleted successfully`,
+            data: {
+                indexDeleted: selectedUserIndex
+            }
+        })
+    } else {
+        res.status(404).json({
+            message: `User with id ${id} not found`
+        });
+    }
+})
+
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
 });
